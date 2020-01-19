779592e6041518b08411e059b96bbf032258ba94
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-01-19 14:04:13 -0300

    aula de ifs e elses

--- adivinhacao/adivinhacao.c
+++ adivinhacao/adivinhacao.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NUMERO_DE_TENTATIVAS 5
+
 int main()
 {
     // imprime cabecalho do nosso jogo
     printf("\n**************************************\n");
-    printf("Bem vindo ao nosso jogo de adivinhaçao\n");
+    printf("Bem vindo ao nosso jogo de adivinhacao\n");
     printf("**************************************\n");
 
     int numerosecreto = 42;
 
     int chute;
-
-    printf("Qual e o seu chute? ");
-    scanf("%d", &chute);
-
     int acertou = (chute == numerosecreto);
+    int maior = (chute > numerosecreto);
+    // int menor = (chute < numerosecreto);
 
-    if (acertou)
+    for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++)
     {
-        printf("Parabens! Voce acertou!\n");
-        printf("Jogue de novo, voce e um bom jogador");
-    }
-    else
-    {
-        int maior = (chute > numerosecreto);
-        if (maior)
+        printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+        printf("Qual e o seu chute? ");
+        scanf("%d", &chute);
+
+        if (acertou)
+        {
+            printf("Parabens! Voce acertou!\n");
+            printf("Jogue de novo, voce e um bom jogador\n");
+            break;
+        }
+        else if (maior)
         {
             printf("Seu chute foi maior que o numero secreto\n");
         }
@@ -33,4 +37,5 @@ int main()
             printf("Seu chute foi menor que o numero secreto\n");
         }
     }
+    printf("FIM DO JOGO!");
 }
